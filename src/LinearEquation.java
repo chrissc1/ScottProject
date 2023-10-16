b6189c195a170e1a5c708c26728681951c6e4dec
BT_2N2_22 <sample@bths.edu>
2023-10-16 14:03:06 -0400

    made a lot of progress on LinearEquation.java

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,3 +1,41 @@
 public class LinearEquation {
+    private int x1;
+    private int y1;
+    private int x2;
+    private int y2;
 
+    public LinearEquation(int x1, int y1, int x2, int y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+    public int getX1() {
+        return x1;
+    }
+
+    public int getY1() {
+        return y1;
+    }
+
+    public int getX2() {
+        return x2;
+    }
+
+    public int getY2() {
+        return y2;
+    }
+
+    public double slope() {
+        return (double) (y2 - y1) / (x2 - x1);
+    }
+
+    public double yIntercept() {
+        return y2 - (slope() * x2);
+    }
+
+    public void getEquationIntcpt() {
+        String equation = "y = " + slope() + "x + " + yIntercept();
+
+    }
 }
