ba986e5385531b9ab27f2c4365507b2d3110e3dc
BT_2N2_22 <sample@bths.edu>
2023-10-17 13:50:21 -0400

    did a lot more things

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class LinearEquation {
     private int x1;
     private int y1;
--- src/LinearEquationLogic.java
+++ src/LinearEquationLogic.java
@@ -1,9 +1,24 @@
 import java.util.Scanner;
 
 public class LinearEquationLogic {
+    private LinearEquation coordinate1;
+    private LinearEquation coordinate2;
+
+    private Scanner scan;
 
     public LinearEquationLogic() {
-        Scanner myScanner = new Scanner(System.in);
+        coordinate1 = null;
+        coordinate2 = null;
+        scan = new Scanner(System.in);
+    }
+
+    public void start() {
+        getCoordinates();
+    }
+
+    public void getCoordinates() {
+        System.out.println("Enter Coordinate 1: ");
+
     }
 
 
