4f54cc537f4828ec0ced2adf759bccd09868cc89
BT_2N2_22 <sample@bths.edu>
2023-10-23 13:47:29 -0400

    cool

--- src/LinearEquationLogic.java
+++ src/LinearEquationLogic.java
@@ -1,15 +1,14 @@
 import java.util.Scanner;
 
 public class LinearEquationLogic {
-    private LinearEquation coordinate1;
-    private LinearEquation coordinate2;
+    private String coordinate1;
+    private String coordinate2;
 
-    private Scanner scan;
+    Scanner scan = new Scanner(System.in);
 
     public LinearEquationLogic() {
         coordinate1 = null;
         coordinate2 = null;
-        scan = new Scanner(System.in);
     }
 
     public void start() {
@@ -17,10 +16,16 @@ public class LinearEquationLogic {
     }
 
     public void getCoordinates() {
-        System.out.println("Enter Coordinate 1: ");
-        int coordinate1 = scan.nextInt();
-
-
+        System.out.println("Enter The first X value: ");
+        int xCord1 = scan.nextInt();
+        System.out.println("Enter The first Y value: ");
+        int yCord1 = scan.nextInt();
+        System.out.println("Enter The second X value: ");
+        int xCord2 = scan.nextInt();
+        System.out.println("Enter The second Y value: ");
+        int yCord2 = scan.nextInt();
+        LinearEquation eq = new LinearEquation(xCord1, yCord1, xCord2, yCord2);
+        System.out.println("The two point are:" + "(" + xCord1 + ", " + yCord1 + ")" + "and" + "(" + xCord2 + ", " + yCord2 + ")");
     }
 
 
--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -1,5 +1,11 @@
 import java.util.Scanner;
 
 public class LinearEquationRunner {
+    public static void main(String[] args) {
+        LinearEquationLogic exe = new LinearEquationLogic();
+        exe.getCoordinates();
+        exe.
+
+    }
 
 }
