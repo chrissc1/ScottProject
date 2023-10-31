de6c9efca0ba6066b6de0e268bfcc3f407bb8a89
BT_2N2_12 <sample@bths.edu>
2023-10-31 13:19:14 -0400

    added getEquation method

--- src/LinearEquationLogic.java
+++ src/LinearEquationLogic.java
@@ -1,31 +1,43 @@
 import java.util.Scanner;
 
 public class LinearEquationLogic {
-    private String coordinate1;
-    private String coordinate2;
+    private int xCord1;
+    private int xCord2;
+    private int yCord1;
+    private int yCord2;
 
     Scanner scan = new Scanner(System.in);
 
     public LinearEquationLogic() {
-        coordinate1 = null;
-        coordinate2 = null;
     }
 
     public void start() {
-        getCoordinates();
+        getInfo();
     }
 
     public void getCoordinates() {
         System.out.println("Enter The first X value: ");
-        int xCord1 = scan.nextInt();
+        xCord1 = scan.nextInt();
         System.out.println("Enter The first Y value: ");
-        int yCord1 = scan.nextInt();
+        yCord1 = scan.nextInt();
         System.out.println("Enter The second X value: ");
-        int xCord2 = scan.nextInt();
+        xCord2 = scan.nextInt();
         System.out.println("Enter The second Y value: ");
-        int yCord2 = scan.nextInt();
+        yCord2 = scan.nextInt();
         LinearEquation eq = new LinearEquation(xCord1, yCord1, xCord2, yCord2);
         System.out.println("The two point are:" + "(" + xCord1 + ", " + yCord1 + ")" + "and" + "(" + xCord2 + ", " + yCord2 + ")");
+    }
+    public void getEquation() {
+        int top = yCord2 - yCord1;
+        int bottom = xCord2 - xCord1;
+        double slopeInt = yCord2 - ((top / bottom) * xCord2);
+        if ((yCord2 - yCord1) % (xCord2 - xCord1) != 0) {
+            String slope = top + "/" + bottom;
+            System.out.println("The equation of the line between these points is: y = " + slope + " + " + slopeInt );
+        } else {
+            int slope = top / bottom;
+            System.out.println("The equation of the line between these points is: y = " + slope + " + " + slopeInt );
+        }
     }
 
 
